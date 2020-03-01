099f13b912ba62108e32df79a1a9be3df3c888ed
shubhamjones <60847661+shubhamjones@users.noreply.github.com>
2020-03-01 23:19:07 +0530

    Ans 6

---
+++ Q6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q6
+{
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        try
+        {
+            int n = Integer.parseInt(scn.nextLine());
+            if (99%n == 0)
+                System.out.println(n + "is a factor of 99");
+        } catch (ArithmeticException ex)
+        {
+            System.out.println("Arithmetic Exception " + ex);
+        }
+        catch (NumberFormatException ex)
+        {
+            System.out.println("Number Format Exception " + ex);
+        }
+    }
+}
