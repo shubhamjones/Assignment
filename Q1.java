5cc11fa2d0544819696b3d70638f98a79c094c4f
shubhamjones <60847661+shubhamjones@users.noreply.github.com>
2020-03-01 23:47:19 +0530

    Ans 1

---
+++ Q1.java
@@ -0,0 +1,9 @@
+public class Q1 {
+    public static void main(String args[]) {
+        String str = "Shivam";
+        System.out.println(str.replace('S', 'D'));
+        System.out.println(str.replaceFirst("Sh", "Dd"));
+        System.out.println(str.replaceAll("vam", "nam"));
+    }
+}
+
