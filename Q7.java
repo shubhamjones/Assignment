2e44445028dc175f4cc497f2c5b830857342fefa
shubhamjones <60847661+shubhamjones@users.noreply.github.com>
2020-03-02 00:00:08 +0530

    Ans 7

---
+++ Q7.java
@@ -0,0 +1,50 @@
+
+
+public class Q7 {
+    static String FirstName;
+    static String LastName;
+    static int Age;
+
+    static {
+        FirstName = "Shubham";
+        LastName = "Verma";
+        Age = 23;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("FirstName:" + FirstName);
+        System.out.println("LastName:" + LastName);
+        System.out.println("Age:" + Age);
+    }
+
+}
+/*
+public class Q7{
+    static String var1="Shivam";
+    static String var2="Sahay";
+    static int var3=23;
+    static void disp()
+    {
+        System.out.println("First Name:" + var1);
+        System.out.println("Last Name:" + var2);
+        System.out.println("Age:" + var3);
+    }
+    public static void main(String[] args)
+    {
+     disp();
+    }
+}
+
+public class Q7{
+    static String FirstName="Nivedita";
+    static String LastName="Katiyar";
+    static int Age=23;
+    public static void main(String[] args)
+    {
+        System.out.println("FirstName:" + FirstName);
+        System.out.println("LastName:" + LastName);
+        System.out.println("Age:" +Age);
+    }
+}
+*/
