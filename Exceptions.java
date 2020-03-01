cd0740bf509959dccdc5a31a5d21330b1a0f3e2b
shubhamjones <60847661+shubhamjones@users.noreply.github.com>
2020-03-01 23:15:06 +0530

    Ans 3

---
+++ Exceptions.java
@@ -0,0 +1,27 @@
+//ClassNotFoundException
+/*
+public class Exceptions
+{
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            Class.forName("ddsc");
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
+
+//NoClassDefFoundError
+
+private static Test test=new Test();
+public static void main(String[]args){
+        System.out.println("the definitions of test was found");
+        }
+        }
+
+     */
