758012e3fc6e8b87849b7e80fd4cfd8a61e56334
shubhamjones <60847661+shubhamjones@users.noreply.github.com>
2020-04-27 15:04:51 +0530

    Q3

---
+++ SameDepartAndArrival.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SameDepartAndArrival {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","/home/shubham/Downloads/chromedriver_linux64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://newtours.demoaut.com");
+		driver.findElement(By.xpath("//input[@name='userName']")).sendKeys("shubham.verma1@tothenew.com");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("1234567");
+		driver.findElement(By.xpath("//input[@name='login']")).click();
+//		tripType
+		driver.findElement(By.xpath("//input[@name='tripType']")).click();
+		Select sel = new Select(driver.findElement(By.xpath("//select[@name='passCount']")));
+		sel.selectByValue("2");
+		Select n = new Select(driver.findElement(By.xpath("//select[@name='fromPort']")));
+		n.selectByValue("New York");
+
+		Select m = new Select(driver.findElement(By.xpath("//select[@name='fromMonth']")));
+		m.selectByValue("3");
+
+		Select d = new Select(driver.findElement(By.xpath("//select[@name='fromDay']")));
+		d.selectByValue("27");
+		Select t = new Select(driver.findElement(By.xpath("//select[@name='toPort']")));
+		t.selectByValue("New York");
+		Select mo = new Select(driver.findElement(By.xpath("//select[@name='toMonth']")));
+		mo.selectByValue("3");
+
+		Select da = new Select(driver.findElement(By.xpath("//select[@name='toDay']")));
+		da.selectByValue("29");
+		driver.findElement(By.xpath("//input[@name='servClass']")).click();
+		Select air = new Select(driver.findElement(By.xpath("//select[@name='airline']")));
+		air.selectByVisibleText("Blue Skies Airlines");
+		driver.findElement(By.xpath("//input[@name='findFlights']")).click();
+
+	}
+
+}
